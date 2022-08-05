930b9e6e3f9a965df76494908a807d883a07e932
Matthew Levi <matthewlevi1990@gmail.com>
2022-08-05 08:06:19 +0100

    update

--- src/pages/SubscriptionPage.js
+++ src/pages/SubscriptionPage.js
@@ -7,6 +7,10 @@ export default function SubscriptionPage() {
         <div className="subscription">
           <h2>Thank you for subscribing</h2>
           <p>
+            One more thing: Please, check your mailbox. We've sent you a mail.
+            <br></br> We need to be sure that you dropped the correct email
+          </p>
+          <p>
             Click <Link to="/">here</Link> to go back to the homepage
           </p>
         </div>
