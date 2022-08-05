4b1b184c774180074d14f0e024f258c5ffce1991
blackalbino01 <hauwal4969@gmail.com>
2022-08-05 09:54:48 +0100

    almost done

--- src/pages/DescriptionPage.js
+++ src/pages/DescriptionPage.js
@@ -15,15 +15,16 @@ import {
 import { useEffect, useState } from "react";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
+import ArtData from "../ArtData";
 var base64 = require("base-64");
 
 export default function DescriptionPage() {
   const params = useParams();
-  const [buyButtonText, setBuyButtonText] = useState("BUY");
-  const [art, setArt] = useState("");
   const [isSold, setIsSold] = useState(false);
   const [load, setLoad] = useState(false);
-
+  const artImage = ArtData.find(item => {
+    return item.id == params.key
+ })
   const providerOptions = {
     walletconnect: {
       package: WalletConnectProvider,
@@ -40,31 +41,9 @@ export default function DescriptionPage() {
   });
 
   useEffect(() => {
-    fetchMetadata();
     checkStatus();
   }, []);
 
-  const fetchMetadata = async () => {
-    console.log("fetching nft");
-    const provider = new ethers.providers.JsonRpcProvider(PROVIDER);
-    const contract = new ethers.Contract(
-      CONTRACT_ADDRESS,
-      TwoBrothersAndOneLumbo.abi,
-      provider
-    );
-
-    const tokenUri = await contract.tokenURI(params.key);
-    const json = base64.decode(tokenUri.substring(29));
-    const meta = JSON.parse(json);
-    setArt({
-      price: meta.price,
-      artist: meta.artist,
-      img: meta.img,
-      title: meta.title,
-      description: meta.description,
-    });
-  };
-  fetchMetadata();
 
   const checkStatus = async () => {
     const provider = new ethers.providers.JsonRpcProvider(PROVIDER);
@@ -91,7 +70,7 @@ export default function DescriptionPage() {
       signer
     );
     const usdt = new ethers.Contract(USDT, ERC20.abi, signer);
-    await usdt.approve(CONTRACT_ADDRESS, price);
+    await usdt.approve(CONTRACT_ADDRESS, ethers.utils.parseUnits(price.toString()));
     setLoad(true);
     const transaction = await contract.buyItem(id);
     await transaction.wait();
@@ -103,11 +82,11 @@ export default function DescriptionPage() {
       <section className="container">
         <section className="description-container">
           <div className="description-image-container">
-            <img src={`${art.img}`} className="description-image" alt="" />
+            <img src={`${artImage.img}`} className="description-image" alt="" />
           </div>
           <div className="description">
             <header className="description-header">
-              <h1>{art.title}</h1>
+              <h1>{artImage.title}</h1>
             </header>
 
             <div className="art-description-details">
@@ -119,7 +98,7 @@ export default function DescriptionPage() {
                   ></FontAwesomeIcon>{" "}
                   Artist
                 </p>
-                <p className="artist-name">{art.artist}</p>
+                <p className="artist-name">{artImage.artist}</p>
               </div>
               <div className="art-ownership">
                 <p className="description-art-tag">
@@ -129,7 +108,7 @@ export default function DescriptionPage() {
                   ></FontAwesomeIcon>{" "}
                   Price
                 </p>
-                <p className="artist-name">{art.price} USDT</p>
+                <p className="artist-name">{artImage.price} USDT</p>
                 {load ? (
                   <button className="buy-button">{"Loading..."}</button>
                 ) : isSold ? (
@@ -137,7 +116,7 @@ export default function DescriptionPage() {
                 ) : (
                   <button
                     className="buy-button"
-                    onClick={() => buyItem(params.key, art.price)}
+                    onClick={() => buyItem(params.key, artImage.price)}
                   >
                     {"BUY"}
                   </button>
@@ -164,7 +143,7 @@ export default function DescriptionPage() {
               ></FontAwesomeIcon>{" "}
               Description
             </h1>
-            <p className="art-description">{art.description}</p>
+            <p className="art-description">{artImage.description}</p>
           </div>
         </section>
       </section>
