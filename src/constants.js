0b62c9b6abd959f9c43fc7fd33a108c898125474
MXCII-007 <110568330+MXCII-007@users.noreply.github.com>
2022-08-05 12:00:20 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const CONTRACT_ADDRESS = "0x6615b799A58b850351Fd383EF02160BF88F2039c"
 const USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-const PROVIDER = "https://quiet-twilight-smoke.rinkeby.discover.quiknode.pro/d8f3f4148d07a0f521ec4db39fe487a4ea784fad"
+const PROVIDER = "https://long-autumn-wind.discover.quiknode.pro/e9633673adc3b1ddc2c9082eb5305cd30609b85c"
 
 export {CONTRACT_ADDRESS,USDT, PROVIDER};
