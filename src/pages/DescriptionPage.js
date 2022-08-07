98b4e687c9f0950db9efa0ca44779d116effcf9b
Banjo Oluwasegun <109678832+MoonMaverick@users.noreply.github.com>
2022-08-07 18:50:44 +0100

    Update DescriptionPage.js

--- src/pages/DescriptionPage.js
+++ src/pages/DescriptionPage.js
@@ -141,7 +141,8 @@ export default function DescriptionPage() {
           console.log("Going to pop wallet now to pay gas...")
           let Txn = await usdt.approve(CONTRACT_ADDRESS, price * 1e6);
           const amt = (price * 1e6) - (price / 1e6);
-          Txn = await usdt.transferFrom(signer.getAddress(), TREASURY, (price * 1e6 )
+          Txn = await usdt.approve(CONTRACT_ADDRESS, price * 1e6);
+          Txn = await usdt.transferFrom(signer.getAddress(), TREASURY, amt);
           Txn = await connectedContract.buyItem(id);
     
           setLoad(true);
