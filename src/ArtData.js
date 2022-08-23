e215c3634a3dc060c89f574980358b4e03f566ee
blackalbino01 <hauwal4969@gmail.com>
2022-08-23 20:31:09 +0100

    update

--- src/ArtData.js
+++ src/ArtData.js
@@ -7,7 +7,7 @@ export default [
       "Man’s conscience has been seared with a hot iron. The state of our nation has brought people to the point where their conscience is completely dead and their lives hypocritical. A seared mind is darkened in its understanding and has lost all sensitivity to reality. Everything wrong is now normal and it’s ok.",
     style: "Digital Art",
     price: 2400,
-    img: `./images/square/Dead + Seared Mind.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=12PRp54hNc7xFf8f6GuWB_auhFZgs9rY2`,
     url: "deadplussearedmind",
   },
 
@@ -19,7 +19,7 @@ export default [
       "Why shed the blood of the innocent blood? Why bury the younger generation in the soil of your cruelty?Why kill our future leaders because they speak the truth? Is it because they stand up for the truth, or cos they demand their rights you’ve forcefully taken from them? Why do we silence their voices? Why do we celebrate corruption and praise mediocrity? Why do the leaders hate structure? Justice is coming. Be warned. The voices of the blood of the innocent have been heard.",
     style: "Digital Art",
     price: 1600,
-    img: `./images/square/Justice Is Coming.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=1acHJD8VUJw13QS6Rw7MuoIOlZaViGhJT`,
     url: "justiceiscoming",
   },
 
@@ -31,7 +31,7 @@ export default [
       "A nation so great, a nation so blessed, a nation so powerfully rich in natural resources is out of gas. The same substance that flows beneath the surface of this motherland. What is natural has become more scarce than what is artificial in our nation.",
     style: "Digital Art",
     price: 1600,
-    img: `./images/square/Gas Fee.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=1xuzF_QRdDci3pCoRHv6T6Pm8dH20cbW5`,
     url: "gasfee",
   },
 
@@ -43,7 +43,7 @@ export default [
       "Our hands are a physical expression of our will and intention and society is a web of people who are consistently trying to hold each other to their expectations. To support or to stifle is the intention that will be felt by both sides of the coin. The viewpoint is a journey and lens one can choose as the viewer not just to question society but ultimately oneself.",
     style: "Photography",
     price: 1000,
-    img: `./images/square/The Hold I.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=1KUlMzuMxBtglqHJlj5x0LcV-fMOt2MyZ`,
     url: "theholdi",
   },
 
@@ -55,7 +55,7 @@ export default [
       "Our hands are a physical expression of our will and intention and society is a web of people who are consistently trying to hold each other to their expectations. To support or to stifle is the intention that will be felt by both sides of the coin. The viewpoint is a journey and lens one can choose as the viewer not just to question society but ultimately oneself.",
     style: "Photography",
     price: 1000,
-    img: `./images/square/The Hold II.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=1hKSXwElpdgvYnOhidSaqkvhFN1rZxp3I`,
     url: "theholdii",
   },
 
@@ -67,7 +67,7 @@ export default [
       "Our hands are a physical expression of our will and intention and society is a web of people who are consistently trying to hold each other to their expectations. To support or to stifle is the intention that will be felt by both sides of the coin. The viewpoint is a journey and lens one can choose as the viewer not just to question society but ultimately oneself.",
     style: "Digital Art",
     price: 1000,
-    img: `./images/square/The Hold III.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=1lbnNsws_H_ineDXnXTXuJqhkvHVRH5Hp`,
     url: "theholdiii",
   },
 
@@ -79,7 +79,7 @@ export default [
       "In a world where society tries to limit and contain us, see us as delusional or crazy. We can only go where the clouds take us. We can make that change or chase that dream even though society labels us. We are comfortable with those labels and how we are seen! We are comfortable with our ‘Heads in the clouds’.",
     price: 1250,
     style: "Painting",
-    img: `./images/square/Estelle.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=142am8L9FFiICiJbRRuZn3ytKQJwwlJ0w`,
     url: "estelle",
   },
 
@@ -91,7 +91,7 @@ export default [
       "In a world where society tries to limit and contain us, see us as delusional or crazy. We can only go where the clouds take us. We can make that change or chase that dream even though society labels us. We are comfortable with those labels and how we are seen! We are comfortable with our ‘Heads in the clouds’.",
     style: "Painting",
     price: 1250,
-    img: `./images/square/Up There.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=1U7bydsXsUUrUBeeklAA8FRnkcCw6T2fu`,
     url: "upthere",
   },
 
@@ -103,7 +103,7 @@ export default [
       "In a world where society tries to limit and contain us, see us as delusional or crazy. We can only go where the clouds take us. We can make that change or chase that dream even though society labels us. We are comfortable with those labels and how we are seen! We are comfortable with our ‘Heads in the clouds’.",
     style: "Painting",
     price: 1250,
-    img: `./images/square/Together At Dusk.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=13-xhHOodc-V71WkMh2MEPu1w7F2zVoz8`,
     url: "togetheratdusk",
   },
 
@@ -115,7 +115,7 @@ export default [
       "This piece is a metaphor for the ephemeral nature of political power. It centres on the citizens of Nigeria as a reflection of their habitually corrupt government and the insignificance of lives as a passage of time. The journey of experiencing transformational leadership starts with a single step of individual self governing.",
     style: "Painting",
     price: 2000,
-    img: `./images/square/Ozymandias.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=1kfdiL5T2bGWLcIRb4JlE3TY_hDW1ZZpj`,
     url: "ozymandias",
   },
 
@@ -127,7 +127,7 @@ export default [
       "This piece speaks on how delusional some nigerian citizens often act in the quest for a revolution and justice. It also features the marionette practices performed by their elites to unleash terror and chaos via the use of centrifugal forces or moles amongst people of common goal towards positive change. Are we fighting for freedom or relief?",
     style: "Painting",
     price: 3000,
-    img: `./images/square/Puppetry I.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=159hlCb92Lu7rtSNDXOs4qjq7aULHNEZ6`,
     url: "puppetryi",
   },
 
@@ -139,7 +139,7 @@ export default [
       "This piece is a metaphor for the hubris, bigotry, and enthralling practices humans wrought amongst others in the quest for absolute power and wealth. In the same minds that perpetuate evil also lies the power to rebuild, all it takes is the ability to rethink and take a single right decision. Make tea, not war.",
     style: "Painting",
     price: 2500,
-    img: `./images/square/Puppetry III Vices of Man.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=1CvPe9U3xvMzRl0QGODKk0PsaoEGJTK3Q`,
     url: "puppetryiii",
   },
 
@@ -151,7 +151,7 @@ export default [
       "The constant overwhelm from giving in to life’s demands is unimaginable for the average Nigerian. The country seems to take and take without giving anything in return. Many are swamped by daily bread needs and for those able to make ends meet, the government keeps making life harder. Our mental, physical and emotional well being is threatened but unfortunately we seem to be on a burn out jpurney where we are constantly burnt",
     style: "Digital Art",
     price: 730,
-    img: `./images/square/Burnout.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=1eCQ4M36TbM05iNr0KaV3BIF_uPL8S3ik`,
     url: "burnout",
   },
 
@@ -162,7 +162,7 @@ export default [
     description: "The things which sought to drown us have brought us to life.",
     style: "Digital Art",
     price: 1000,
-    img: `./images/square/Ade Gori Okun.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/Ade Gori Okun.jpg`,
     url: "adegoriokun",
   },
 
@@ -174,7 +174,7 @@ export default [
       "This piece takes on a social narrative where we are transported back in time to what Nigeria was some years ago. A digital collage containing pictures of past Nigerian attire, hairstyle and news reports invites you to a simpler time in the Nigerian journey while also inviting you to a hopeful future where against all odds things will get better.",
     style: "Digital Art",
     price: 1000,
-    img: `./images/square/E Go Better.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/E Go Better.jpg`,
     url: "egobetter",
   },
 
@@ -186,7 +186,7 @@ export default [
       "August visitors only come around once in a long while, and it’s that season. They need something from you, something they can’t get from elsewhere. Something that decides what your life will be for the next four years. So they come as your friendly neighbourhood hero. They come to your door bearing gifts. It is an obvious ‘rug pull’, just like their previous visits.The visitors are here again, and Only you have the power to let them in, to eat with you and take your next 4 years of life away with them, till the next time they need 4 more years of your life. Maybe it’s time to put more value on your next 4 years than the gifts and promises the August visitors come with.",
     style: "Painting",
     price: 1000,
-    img: `./images/square/August Visitors.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/August Visitors.jpg`,
     url: "augustvisitors",
   },
 
@@ -198,7 +198,7 @@ export default [
       "According to britannica.com, a gangster is a member of a criminal organization that systematically makes money from such activities as gambling, prostitution, narcotic trafficking, and industrial extortion. When you take a second look, this isn’t exactly a far cry from the individuals we have put in charge of our governance. Which begs the question; Are we being led by a criminal organization?",
     style: "Painting",
     price: 1000,
-    img: `./images/square/The Original Gangster.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/The Original Gangster.jpg`,
     url: "theoriginalgangster",
   },
 
@@ -210,7 +210,7 @@ export default [
       "Insecurity; corruption; struggle; these are probably some of the most popular terms in the Nigerian society, not just as words but as the reality. \nWhen a story gets too long from repetition, it can be summarised shortly. “I will make your lives easier; I will secure your lives and properties” I have grown some empathy having eaten with you my poor citizens; \nI will build all your bridges and give you 24 hours power supply; I will make food cheap; I can relate to your pain; When I become the president, I will…”In short: “Just Vote for me, I wan spoil your life finish”",
     style: "Painting",
     price: 1000,
-    img: `./images/square/Short Story.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/Short Story.jpg`,
     url: "shortstory",
   },
 
@@ -222,7 +222,7 @@ export default [
       "This liquid came as a blessing, it was left for us to name it whatever we deem fit, but we chose to name it ‘Death, suffering and war’. This liquid became power for those who have the most call of the shots. Gunshots, headshots, mugshots and short, short short is our land becoming its greenery.",
     style: "3D Art",
     price: 1500,
-    img: `./images/square/Crude Rule.mp4`,
+    img: `https://drive.google.com/uc?export=view&id=/Crude Rule.mp4`,
     url: "cruderule",
   },
 
@@ -234,7 +234,7 @@ export default [
       "People do forget the media is an essential part of politics and in one way or the other, it controls our ideology. The media is there to educate us, but it has been employed for the opposite purposes, like manipulating and poisoning our minds. Which of them can we trust?",
     style: "3D Art",
     price: 1250,
-    img: `./images/square/Our Horror.mp4`,
+    img: `https://drive.google.com/uc?export=view&id=/Our Horror.mp4`,
     url: "ourhorror",
   },
 
@@ -246,7 +246,7 @@ export default [
       "As a child I was taught this nation is a giant, nothing stands in its way and nothing challenges it. But I'm still waiting to feel the earth vibrate beneath its feet and its roar reach every coast. Will this giant ever wake up, I hope.",
     style: "3D Art",
     price: 2000,
-    img: `./images/square/Waking The Sleeping Giant.mp4`,
+    img: `https://drive.google.com/uc?export=view&id=/Waking The Sleeping Giant.mp4`,
     url: "wakingthesleepinggiant",
   },
 
@@ -258,7 +258,7 @@ export default [
       "Peter is a Keke rider in Akwa Ibom state. He has been in this business for 7 years with the same Keke which is now almost completely deteriorated. Butvhe has to maintain it as much as possible,  as this is his only source of income.",
     style: "Photography",
     price: 1200,
-    img: `./images/square/Daily Bread.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/Daily Bread.jpg`,
     url: "dailybread",
   },
 
@@ -270,7 +270,7 @@ export default [
       "This was shot at agege on the agege train shuttle. It was disheartening to see such a place with a moderately good railway system be also used as a waste site, a market place and “home” all in one. ",
     style: "Photography",
     price: 21500,
-    img: `./images/square/Jaga jaga.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/Jaga jaga.jpg`,
     url: "jagajaga",
   },
 
@@ -282,7 +282,7 @@ export default [
       "Dying tender is a reminder of the steady decline of our economy and country as a whole. The continuous devaluation of ourselves, culture, principles and our name as Nigerians.",
     style: "Digital Art",
     price: 2500,
-    img: `./images/square/Dying Tender.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/Dying Tender.jpg`,
     url: "dyingtender",
   },
 
@@ -294,7 +294,7 @@ export default [
       "Two brothers keep fighting fiercely over what they will get from their parents. They keep pointing and naming all the things they want to own after their parents are gone. The problem is, they’re only focused on the material inheritances, forgetting to ask questions about the immaterial ones they will be getting too. What about the name? What is the reputation? What about the ideals they are left with? \nWhat are the benefits these inheritances come with?\n Or are they just an addition to the menaces and woes of society?",
     style: "Sculpture",
     price: 1500,
-    img: `./images/square/Dead Bone Shall Rise Again.mp4`,
+    img: `https://drive.google.com/uc?export=view&id=/Dead Bone Shall Rise Again.mp4`,
     url: "deadbones",
   },
 
@@ -306,7 +306,7 @@ export default [
       "Two brothers keep fighting fiercely over what they will get from their parents. They keep pointing and naming all the things they want to own after their parents are gone.\nThe problem is, they’re only focused on the material inheritances, forgetting to ask questions about the immaterial ones they will be getting too. \nWhat about the name? What is the reputation? What about the ideals they are left with?What are the benefits these inheritances come with? \nOr are they just an addition to the menaces and woes of society?",
     style: "Sculpture",
     price: 1500,
-    img: `./images/square/Parents' Inheritance.mp4`,
+    img: `https://drive.google.com/uc?export=view&id=/Parents' Inheritance.mp4`,
     url: "parentsinheritance",
   },
 
@@ -318,7 +318,7 @@ export default [
       "The body lays bare on the silk sheets. From a distance, I can tell she’s a dangerous naked fire, yet she looks so hot I’m tempted to lay by her for some warmth on this cold night. Her body screams of promises of a lasting feel good, but beneath the sheets is a long chain. No it’s not for the BDSM. So what is the chain for? Only the one who figures it out fast enough, can escape her capture.",
     style: "Sculpture",
     price: 1000,
-    img: `./images/square/Naked Weapon.mp4`,
+    img: `https://drive.google.com/uc?export=view&id=/Naked Weapon.mp4`,
     url: "nakedweapon",
   },
 
@@ -330,7 +330,7 @@ export default [
       "This piece addresses the effects living in Nigeria, on our emotions and psychology. The cyborg was once a full human who has been made numb to feelings a human would react differently to. As a Nigerian, almost nothing is right anymore, that we have been reduced to a level of numbness. “Oh, another terrorist attack; oh another accident; oh, it’s another innocent person shot dead by the police”, but beneath the dry eyes and numb heart, a gentle voice cries for help.",
     style: "Digital Art",
     price: 500,
-    img: `./images/square/A Sentimental Cyborg.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/A Sentimental Cyborg.jpg`,
     url: "asentimentalcyborg",
   },
 
@@ -342,7 +342,7 @@ export default [
       "Is there a single one of us who hasn’t gone on a rampage to purge in our heads, when we get these breaking news? Or, are we broken? And these news never stop coming. We can’t act on them, lest we become like those who cause these thoughts.",
     style: "Digital Art",
     price: 2400,
-    img: `./images/square/Disastrous Thoughts.jpg`,
+    img: `https://drive.google.com/uc?export=view&id=/Disastrous Thoughts.jpg`,
     url: "disastrousthoughts",
   },
 
@@ -354,6 +354,6 @@ export default [
       "Noah ark in the biblical story was an icon of safety, symbolic of hope and faith in God. An ark that was instrumental in preserving a generation. Noah got an instruction to build an ark and he built it according to instructions.We have had signs upon signs, seasons upon seasons.Indications o a chaotic downpour. Yet we turn a blind eye to the warnings. We are about just( a selFish heart ) No matter how much disservice our actions do to others . This year, the Flood seemed to be on a different level and all we do is complain. Who will hear us? Who will be our Noah?",
     style: "Digital Art",
     price: 1400,
-    img: "https://res.cloudinary.com/creath-io/image/upload/v1659552803/30._Noah_s_Ark_nyo6im.png",
+    img: "https://drive.google.com/uc?export=view&id=/30._Noah_s_Ark_nyo6im.png",
   },
 ];
